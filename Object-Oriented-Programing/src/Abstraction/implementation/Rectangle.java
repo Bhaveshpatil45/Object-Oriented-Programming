6a941221e0cfde6b91a745ea48aeb5d43398c8ca
Bhaveshpatil45 <bp700259@gmail.com>
2024-10-22 13:49:16 +0530

    Oops

---
+++ Object-Oriented-Programing/src/Abstraction/implementation/Rectangle.java
@@ -0,0 +1,8 @@
+package Abstraction.implementation;
+
+public class Rectangle extends AbstractShape {
+    @Override
+    public void draw() {
+        System.out.println("rectangle drawn.");
+    }
+}
